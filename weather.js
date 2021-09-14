f1a3af176148a46dca5338d8d99d3ee0e45edb32
Avisikta24 <88192640+Avisikta24@users.noreply.github.com>
2021-09-14 22:11:32 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,23 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=88fb1f7c04de2fab0610b1d7a43f58d3')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Description - "+descValue;
+  temp.innerHTML = "Temperature - "+tempValue;
+  input.value ="";
+})
+.catch(err => alert("Wrong city name!"));
+})
